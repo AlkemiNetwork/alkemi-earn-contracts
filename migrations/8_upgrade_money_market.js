29db275c63f285f058b996c11609565f926f2602
Raghu <ragshri@gmail.com>
2020-12-09 20:42:15 -0500

    Origination Fee credit back as admin to ensure total supply is equal to cash

--- migrations/8_upgrade_money_market.js
+++ migrations/8_upgrade_money_market.js
@@ -1,12 +1,11 @@
 // migrations/4_prepare_upgrade_boxv2.js
 const MoneyMarket = artifacts.require("MoneyMarket");
-const MoneyMarketV2 = artifacts.require("MoneyMarketV2");
 
 const { prepareUpgrade } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer) {
 	const moneyMarket = await MoneyMarket.deployed();
-	await prepareUpgrade(moneyMarket.address, MoneyMarketV2, {
+	await prepareUpgrade(moneyMarket.address, MoneyMarket, {
 		deployer,
 		unsafeAllowCustomTypes: true,
 	});
