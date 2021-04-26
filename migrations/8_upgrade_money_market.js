d4f42c40bc09e9bcac146d2a965c14e45d660060
Raghu <ragshri@gmail.com>
2021-04-26 18:44:32 -0400

    Clearing unit testing issue PROTO-47

--- migrations/8_upgrade_money_market.js
+++ migrations/8_upgrade_money_market.js
@@ -5,9 +5,9 @@ const MoneyMarketV12 = artifacts.require("MoneyMarketV12");
 const { prepareUpgrade } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer) {
-	const moneyMarket = await MoneyMarket.deployed();
-	await prepareUpgrade(moneyMarket.address, MoneyMarketV12, {
-		deployer,
-		unsafeAllowCustomTypes: true,
-	});
+	// const moneyMarket = await MoneyMarket.deployed();
+	// await prepareUpgrade(moneyMarket.address, MoneyMarketV12, {
+	// 	deployer,
+	// 	unsafeAllowCustomTypes: true,
+	// });
 };
