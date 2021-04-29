693785422bffc0787697b057457e9f5e8e2d160b
Part Saelim <part@atato.com>
2021-04-29 15:29:56 +0700

    Initially add rewardControl contract logic for claiming ALK (2)

--- contracts/RewardControl.sol
+++ contracts/RewardControl.sol
@@ -3,93 +3,91 @@ pragma solidity ^0.4.24;
 
 contract RewardControl {
 
-    uint ALK_REWARDS_ISSUED_PER_BLOCK = 8.323820396;
-    uint TGE_REWARD_BLOCK_NUMBER = 100;
-    MoneyMarket moneyMarket;
-    address[] allMarkets = [WBTC, DAI, ETH, USDC];
-    mapping(address => uint)  alreadyClaimALKForLender;
-    mapping(address => uint)  alreadyClaimALKForBorrower;
-    mapping(address => mapping(address => uint))  alreadyClaimALKByHolderByMarketForLender;
-    mapping(address => mapping(address => uint))  alreadyClaimALKByHolderByMarketForBorrower;
+    uint constant public ALK_REWARDS_ISSUED_PER_BLOCK = 8.323820396;
+    uint constant public TGE_REWARD_BLOCK_NUMBER = 1;
+    uint constant public DOUBLE_SCALE = 1e10;
+    // WBTC, ETH, DAI, USDC
+    address[] constant public ALL_MARKETS = [0x0000000000000000000000000000000000000001, 0x0000000000000000000000000000000000000002, 0x0000000000000000000000000000000000000003, 0x0000000000000000000000000000000000000004];
+
+    mapping(address => uint)  alreadyClaimedRewards;
+    MoneyMarket moneyMarket; // Money Market Contract
 
     constructor() public {
 
     }
 
-    function calculateAccruedRewards(address holder) public view returns(uint) {
-        // @TODO
-        uint alkAccrued = 0
-        Market[] markets = allMarkets
-        for (uint i = 0; i < markets.length; i++) {
-            Market currentMarket = markets[i];
-            require(isMarketSupport(currentMarket), "market must be launched in reward program");
-            // claim lending rewards
+    // calculate accrued rewards since TGE block
+    function calculateAccruedRewards(address holder) public view returns (uint) {
+        uint totalAccruedRewards = 0;
 
-            uint rewardsAllocationRatioForTheMarket = getRewardAllocationRatioForMarkets(currentMarket); // 25
-            uint marketTotalRewardsPerBlock = rewards(ALK_REWARDS_ISSUED_PER_BLOCK * rewardsAllocationRatioForTheMarket) / 100; // 2.080955099
-            uint marketLendersRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
-            uint marketBorrowsRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
+        // calculate block number delta
+        uint blockNumber = getBlockNumber();
+        uint deltaBlocks = blockNumber - TGE_REWARD_BLOCK_NUMBER;
 
-            // calculate block number delta
-            uint blockNumber = getBlockNumber();
-            uint deltaBlocks = blockNumber - TGE_REWARD_BLOCK_NUMBER;
+        if (deltaBlocks > 0) {
+            for (uint i = 0; i < ALL_MARKETS.length; i++) {
+                address currentMarket = ALL_MARKETS[i];
 
-            if (deltaBlocks > 0) {
-                // calculate lending rewards
+                // calculate rewards per block for lending and borrowing
+                uint rewardsAllocationRatioForCurrentMarket = getRewardAllocationRatioForMarket(currentMarket); // 0.25
+                uint marketTotalRewardsPerBlock = (ALK_REWARDS_ISSUED_PER_BLOCK * rewardsAllocationRatioForCurrentMarket); // 2.080955099
+                uint marketLendersRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
+                uint marketBorrowsRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
+
+                // calculate lending rewards for the current market
                 uint lenderCurrentMarketSupply = moneyMarket.getSupplyBalance(holder, currentMarket);
                 uint totalCurrentMarketSupplyLiquidity = moneyMarket.markets[currentMarket].totalSupply;
-                uint lenderALKRewardsPerBlockRatio = lenderCurrentMarketSupply / totalCurrentMarketSupplyLiquidity; // 40
-                uint lenderALKRewardsPerBlock = (marketLendersRewardsPerBlock * lenderALKRewardsPerBlockRatio) / 100; // 0.4161910198
-                uint alkUnclaimedForLender = 0;
+                uint lenderALKRewardsPerBlockRatio = lenderCurrentMarketSupply / totalCurrentMarketSupplyLiquidity; // 0.4
+                uint lenderALKRewardsPerBlock = marketLendersRewardsPerBlock * lenderALKRewardsPerBlockRatio; // 0.4161910198
+                lenderAccruedRewardsForCurrentMarket = (deltaBlocks * lenderALKRewardsPerBlock);
 
-                alkAccruedForLender = deltaBlocks * lenderALKRewardsPerBlock;
-
-                // calculate borrowing rewards
+                // calculate borrowing rewards for the current market
                 uint borrowCurrentMarketSupply = moneyMarket.getBorrowBalance(holder, currentMarket);
                 uint totalCurrentMarketBorrowLiquidity = moneyMarket.markets[currentMarket].totalBorrows;
-                uint borrowALKRewardsPerBlockRatio = borrowCurrentMarketSupply / totalCurrentMarketBorrowLiquidity; // 40
-                uint borrowALKRewardsPerBlock = (marketBorrowsRewardsPerBlock * borrowALKRewardsPerBlockRatio) / 100; // 0.4161910198
-                uint alkUnclaimedForBorrower = 0;
+                uint borrowALKRewardsPerBlockRatio = borrowCurrentMarketSupply / totalCurrentMarketBorrowLiquidity; // 0.4
+                uint borrowALKRewardsPerBlock = marketBorrowsRewardsPerBlock * borrowALKRewardsPerBlockRatio; // 0.4161910198
+                borrowerAccruedRewardsForCurrentMarket = deltaBlocks * borrowALKRewardsPerBlock;
 
-                alkAccruedForBorrower = deltaBlocks * borrowALKRewardsPerBlock;
-
-                alkAccrued = alkAccrued + alkAccruedForLender + alkAccruedForBorrower;
+                // accumulate current market rewards
+                totalAccruedRewards = totalAccruedRewards + lenderAccruedRewardsForCurrentMarket + borrowerAccruedRewardsForCurrentMarket;
             }
         }
 
-        return alkAccrued;
+        return totalAccruedRewards;
     }
 
-    function calculateUnclaimedRewards(address holder) public view returns(uint) {
-        // @TODO
-        uint alkAccrued = calculateAccruedRewards(holder);
-
-        return alkAccrued - alkAlreadyClaimedReward[holder];
+    function calculateUnclaimedRewards(address holder) public view returns (uint) {
+        uint accruedRewards = calculateAccruedRewards(holder);
+        if (accruedRewards > 0 && accruedRewards > alreadyClaimedRewards[holder]) {
+            return accruedRewards - alreadyClaimedRewards[holder];
+        }
+        return 0;
     }
 
-    function claimReward(address holder, uint claimAmount) public {
-        uint unclaimedReward = calculateUnclaimedRewards
-        require(accruedReward - alkAlreadyClaimedReward > claimAmount , "The given reward amount cannot more than unclaimedReward") ;
-//@TODO
-
-
-        transferALKRewards(holder, rewardAmount);
+    function claimRewards(address holder, uint claimedAmount) public {
+        uint unclaimedRewards = calculateUnclaimedRewards(holder);
+        require(unclaimedRewards >= claimedAmount, "The unclaimed rewards must be greater than or equal to the given claimed amount");
+        alreadyClaimedRewards[holder] = alreadyClaimedRewards[holder] + claimedAmount;
+        transferRewards(holder, claimedAmount);
     }
-
 
 
     function getBlockNumber() public view returns (uint) {
         return block.number;
     }
 
-    function getRewardAllocationRatioForMarkets(address market) internal returns (uint) {// return 25
+    function getRewardAllocationRatioForMarket(address market) internal returns (uint) {// return 0.25
         uint totalLiquidity = 0;
-        for (uint i = 0; i < allMarkets.length; i++) {
-            Market currentMarket = moneyMarket.markets[allMarkets[i]]; // call MoneyMarket contract
+        for (uint i = 0; i < ALL_MARKETS.length; i++) {
+            Market currentMarket = moneyMarket.markets[ALL_MARKETS[i]];
             totalLiquidity = totalLiquidity + currentMarket.totalSupply + currentMarket.totalBorrows;
         }
-        Market targetMarket = moneyMarket.markets[market]; // call MoneyMarket contract
-        return (targetMarket.totalSupply + targetMarket.totalBorrows) / totalLiquidity;
+        Market targetMarket = moneyMarket.markets[market];
+        return (targetMarket.totalSupply + targetMarket.totalBorrows) / totalLiquidity ;
+    }
+
+    function transferRewards(address holder, uint amount) internal {
+        // @TODO
     }
 
 }
