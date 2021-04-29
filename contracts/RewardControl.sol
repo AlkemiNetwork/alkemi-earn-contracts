a85a34e6bf5becad5b992fffdc35cd688a20a1ab
Tanapon Kuljarusin <tanapon@atato.com>
2021-04-29 13:19:55 +0700

    move reward calculation part to calculateALKRewards

--- contracts/RewardControl.sol
+++ contracts/RewardControl.sol
@@ -16,6 +16,62 @@ contract RewardControl {
 
     function calculateALKRewards(address holder) public view returns(uint) {
         // @TODO
+        uint alkTotalAccrued = 0
+        Market[] markets = allMarkets
+        for (uint i = 0; i < markets.length; i++) {
+            Market currentMarket = markets[i];
+            require(isMarketSupport(currentMarket), "market must be launched in reward program");
+            // claim lending rewards
+
+            uint rewardsAllocationRatioForTheMarket = getRewardAllocationRatioForMarkets(currentMarket); // 25
+            uint marketTotalRewardsPerBlock = rewards(ALK_REWARDS_ISSUED_PER_BLOCK * rewardsAllocationRatioForTheMarket) / 100; // 2.080955099
+            uint marketLendersRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
+            uint marketBorrowsRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
+
+            // calculate block number delta
+            uint blockNumber = getBlockNumber();
+            uint deltaBlocks = blockNumber - TGE_REWARD_BLOCK_NUMBER;
+
+            // calculate lending rewards
+            uint lenderCurrentMarketSupply = moneyMarket.getSupplyBalance(holder, currentMarket);
+            uint totalCurrentMarketSupplyLiquidity = moneyMarket.markets[currentMarket].totalSupply;
+            uint lenderALKRewardsPerBlockRatio = lenderCurrentMarketSupply / totalCurrentMarketSupplyLiquidity; // 40
+            uint lenderALKRewardsPerBlock = (marketLendersRewardsPerBlock * lenderALKRewardsPerBlockRatio) / 100; // 0.4161910198
+            uint alkUnclaimedForLender = 0
+
+            if (deltaBlocks > 0) {
+                alkAccruedForLender = deltaBlocks * lenderALKRewardsPerBlock;
+                if (alkAccruedForLender >  alreadyClaimALKForLender[holder]) {
+                    alkUnclaimedForLender = alkAccruedForLender - alreadyClaimALKForLender[holder];
+                }
+            }
+
+            // calculate borrowing rewards
+            uint borrowCurrentMarketSupply = moneyMarket.getBorrowBalance(holder, currentMarket);
+            uint totalCurrentMarketBorrowLiquidity = moneyMarket.markets[currentMarket].totalBorrows;
+            uint borrowALKRewardsPerBlockRatio = borrowCurrentMarketSupply / totalCurrentMarketBorrowLiquidity; // 40
+            uint borrowALKRewardsPerBlock = (marketBorrowsRewardsPerBlock * borrowALKRewardsPerBlockRatio) / 100; // 0.4161910198
+            uint alkUnclaimedForBorrower = 0
+            if (deltaBlocks > 0) {
+                alkAccruedForBorrower = deltaBlocks * borrowALKRewardsPerBlock;
+                if (alkAccruedForBorrower >  alreadyClaimALKForBorrower[holder]) {
+                    alkUnclaimedForBorrower = alkAccruedForBorrower - alreadyClaimALKForBorrower[holder];
+                }
+            }
+            alkTotalAccrued = alkTotalAccrued + alkUnclaimedForLender + alkUnclaimedForBorrower
+        }
+
+        return alkTotalAccrued
+    }
+
+    function claimALK2(address holder) public {
+        alkTotalAccrued = calculateALKRewards(address holder)
+
+        if(alkTotalAccrued > 0)
+            //TODO update claim token amount
+
+            //transfer
+            transfer(holder, alkTotalAccrued)
     }
 
     function claimALK(address holder) public {
