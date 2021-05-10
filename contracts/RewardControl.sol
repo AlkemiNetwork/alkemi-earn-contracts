964d43d39248b7a647d32e663ca21434b2049258
Part Saelim <part@atato.com>
2021-05-10 14:13:34 +0700

    Initially add pseudo code for RewardControl.sol

--- contracts/RewardControl.sol
+++ contracts/RewardControl.sol
@@ -1,93 +1,207 @@
 pragma solidity ^0.4.24;
 
-
 contract RewardControl {
 
-    uint constant public ALK_REWARDS_ISSUED_PER_BLOCK = 8.323820396;
-    uint constant public TGE_REWARD_BLOCK_NUMBER = 1;
-    uint constant public DOUBLE_SCALE = 1e10;
-    // WBTC, ETH, DAI, USDC
-    address[] constant public ALL_MARKETS = [0x0000000000000000000000000000000000000001, 0x0000000000000000000000000000000000000002, 0x0000000000000000000000000000000000000003, 0x0000000000000000000000000000000000000004];
+    // #################################################################################################################
 
-    mapping(address => uint)  alreadyClaimedRewards;
-    MoneyMarket moneyMarket; // Money Market Contract
+    struct MarketState {
+        /// The market's last updated alkBorrowIndex or alkSupplyIndex
+        uint224 index;
 
-    constructor() public {
-
+        /// The block number the index was last updated at
+        uint32 block;
     }
 
-    // calculate accrued rewards since TGE block
-    function calculateAccruedRewards(address holder) public view returns (uint) {
-        uint totalAccruedRewards = 0;
+    /// A list of all markets
+    address[] public allMarkets;
 
-        // calculate block number delta
-        uint blockNumber = getBlockNumber();
-        uint deltaBlocks = blockNumber - TGE_REWARD_BLOCK_NUMBER;
+    /// The rate at which the flywheel distributes ALK, per block
+    uint public alkRate; // 4.16191 = 8.323820396 / 2
 
-        if (deltaBlocks > 0) {
-            for (uint i = 0; i < ALL_MARKETS.length; i++) {
-                address currentMarket = ALL_MARKETS[i];
+    /// The portion of alkRate that each market currently receives
+    mapping(address => uint) public alkSpeeds;
 
-                // calculate rewards per block for lending and borrowing
-                uint rewardsAllocationRatioForCurrentMarket = getRewardAllocationRatioForMarket(currentMarket); // 0.25
-                uint marketTotalRewardsPerBlock = (ALK_REWARDS_ISSUED_PER_BLOCK * rewardsAllocationRatioForCurrentMarket); // 2.080955099
-                uint marketLendersRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
-                uint marketBorrowsRewardsPerBlock = marketTotalRewardsPerBlock / 2; // 1.040477549
+    /// The ALK market supply state for each market
+    mapping(address => MarketState) public alkSupplyState;
 
-                // calculate lending rewards for the current market
-                uint lenderCurrentMarketSupply = moneyMarket.getSupplyBalance(holder, currentMarket);
-                uint totalCurrentMarketSupplyLiquidity = moneyMarket.markets[currentMarket].totalSupply;
-                uint lenderALKRewardsPerBlockRatio = lenderCurrentMarketSupply / totalCurrentMarketSupplyLiquidity; // 0.4
-                uint lenderALKRewardsPerBlock = marketLendersRewardsPerBlock * lenderALKRewardsPerBlockRatio; // 0.4161910198
-                lenderAccruedRewardsForCurrentMarket = (deltaBlocks * lenderALKRewardsPerBlock);
+    /// The ALK market borrow state for each market
+    mapping(address => MarketState) public alkBorrowState;
 
-                // calculate borrowing rewards for the current market
-                uint borrowCurrentMarketSupply = moneyMarket.getBorrowBalance(holder, currentMarket);
-                uint totalCurrentMarketBorrowLiquidity = moneyMarket.markets[currentMarket].totalBorrows;
-                uint borrowALKRewardsPerBlockRatio = borrowCurrentMarketSupply / totalCurrentMarketBorrowLiquidity; // 0.4
-                uint borrowALKRewardsPerBlock = marketBorrowsRewardsPerBlock * borrowALKRewardsPerBlockRatio; // 0.4161910198
-                borrowerAccruedRewardsForCurrentMarket = deltaBlocks * borrowALKRewardsPerBlock;
+    /// The ALK borrow index for each market for each supplier as of the last time they accrued ALK
+    mapping(address => mapping(address => uint)) public alkSupplierIndex;
 
-                // accumulate current market rewards
-                totalAccruedRewards = totalAccruedRewards + lenderAccruedRewardsForCurrentMarket + borrowerAccruedRewardsForCurrentMarket;
-            }
+    /// The ALK borrow index for each market for each borrower as of the last time they accrued ALK
+    mapping(address => mapping(address => uint)) public alkBorrowerIndex;
+
+    /// The ALK accrued but not yet transferred to each user
+    mapping(address => uint) public alkAccrued;
+
+    // #################################################################################################################
+
+    /** updateCompSupplyIndex usage
+     * mintAllowed --> supply
+     * redeemAllowed --> withdraw
+     * seizeAllowed --> liquidateBorrow
+     * transferAllowed --> ???
+     * refreshCompSpeedsInternal (used by refreshCompSpeeds)
+     * claimComp --> claimAlk
+     */
+    function refreshAlkIndexForSupplier(address market, address supplier) public {
+        refreshAlkSpeeds();
+        updateAlkSupplyIndex(market);
+        distributeSupplierAlk(market, supplier);
+    }
+
+    /** updateCompBorrowIndex usage
+     * borrowAllowed --> borrow
+     * repayBorrowAllowed --> repayBorrow
+     * refreshCompSpeedsInternal (used by refreshCompSpeeds)
+     * claimComp --> claimAlk
+     */
+    function refreshAlkIndexForBorrower(address market, address borrower) public {
+        refreshAlkSpeeds();
+        updateAlkBorrowIndex(market);
+        distributeBorrowerAlk(market, borrower);
+    }
+
+    /**
+     * Recalculate and update ALK speeds for all ALK markets
+     */
+    function refreshAlkSpeeds() internal {
+        // Loop through each market to sum total_liquidity
+        total_liquidity = 0
+        foreach market in allMarkets:
+            total_liquidity = total_liquidity + MoneyMarket:market.totalSupply + MoneyMarket:market.totalBorrow
+        // For each market calculate reward_allocation_ratio and then multiply it with alkRate to store it in alkSpeeds mapping
+        foreach market in allMarkets:
+            reward_allocation_ratio = (MoneyMarket:market.totalSupply + MoneyMarket:market.totalBorrow) / total_liquidity
+            alkSpeeds[market] = alkRate * reward_allocation_ratio
+    }
+
+    /**
+     * Accrue ALK to the market by updating the supply index
+     * @param market The market whose supply index to update
+     */
+    function updateAlkSupplyIndex(address market) internal {
+        // Get current market state of the given market
+        supply_state = alkSupplyState[market]
+        // Get current market supply speed
+        supply_speed = alkSpeeds[market]
+        // Get delta number of blocks between the current block and the last visited block
+        current_block_number = getCurrentBlockNumber()
+        delta_blocks = current_block_number - supply_state.block
+        // Get total rewards since last visited block
+        alk_accrued = delta_blocks * supply_speed
+        // Get total supply units for current market
+        market_total_supply = MoneyMarket:market.totalSupply
+        // Calculate rewards per unit
+        alk_accrued_per_unit = alk_accrued / market_total_supply
+        // Calculate new supply index
+        new_supply_index = supply_state.index + alk_accrued_per_unit
+        // Update market state
+        alkSupplyState[market] = MarketState({
+                index: new_supply_index,
+                block: current_block_number
+        });
+    }
+
+    /**
+     * Accrue ALK to the market by updating the borrow index
+     * @param market The market whose borrow index to update
+     * @param Exp memory marketBorrowIndex ???
+     */
+    function updateAlkBorrowIndex(address market) internal {}
+
+    /**
+     * Calculate ALK accrued by a supplier and add it on top of alkAccrued[supplier]
+     * @param market The market in which the supplier is interacting
+     * @param supplier The address of the supplier to distribute ALK to
+     */
+    function distributeSupplierAlk(address market, address supplier) internal {
+        // Get current market state of the given market
+        supply_state = alkSupplyState[market]
+        supply_index = supply_state.index
+        // Get the last snapshot of the supplier index for the given market
+        supplier_index = alkSupplierIndex[market][supplier]
+        // Update snapshot of the supplier index for the given market to the latest index
+        alkSupplierIndex[market][supplier] = supply_index
+
+        if supplier_index is not initialized:
+           // Set supplier_index to the initial value
+
+        delta_index = supply_index - supplier_index
+        // Get the balance of the given market for the supplier address
+        supplier_balance = MoneyMarket:getSupplyBalance(supplier, market)
+        // Calculate ALK accrued to be distributed to the particular supplier
+        supplier_delta = supplier_balance * delta_index
+        alkAccrued[supplier] = alkAccrued[supplier] + supplier_delta
+    }
+
+    /**
+     * Calculate ALK accrued by a borrower and add it on top of alkAccrued[borrower]
+     * @dev Borrowers will not begin to accrue until after the first interaction with the protocol.
+     * @param market The market in which the borrower is interacting
+     * @param borrower The address of the borrower to distribute ALK to
+     */
+    function distributeBorrowerAlk(address market, address borrower, Exp memory marketBorrowIndex) internal {}
+
+    /**
+     * Calculate additional accrued ALK for a contributor since last accrual
+     * @param contributor The address to calculate contributor rewards for
+     */
+    /*function updateContributorRewards(address contributor) public {}*/
+
+    /**
+     * @notice Claim all the ALK accrued by holder in all markets
+     * @param holder The address to claim ALK for
+     */
+    function claimAlk(address holder) public {
+        return claimAlk(holder, allMarkets);
+    }
+
+    /**
+     * @notice Claim all the ALK accrued by holder in the specified markets
+     * @param holder The address to claim ALK for
+     * @param markets The list of markets to claim ALK in
+     */
+    function claimAlk(address holder, address[] memory markets) internal {
+        // loop through each market
+        for (uint i = 0; i < markets.length; i++) {
+            address market = markets[i];
+
+            updateAlkSupplyIndex(market);
+            distributeSupplierAlk(market, holder);
+
+            updateAlkBorrowIndex(market);
+            distributeBorrowerAlk(market, holder);
+
+            alkAccrued[supplier] = transferAlkInternal(holder, alkAccrued[supplier]);
         }
-
-        return totalAccruedRewards;
     }
 
-    function calculateUnclaimedRewards(address holder) public view returns (uint) {
-        uint accruedRewards = calculateAccruedRewards(holder);
-        if (accruedRewards > 0 && accruedRewards > alreadyClaimedRewards[holder]) {
-            return accruedRewards - alreadyClaimedRewards[holder];
-        }
-        return 0;
-    }
+    /**
+     * Transfer ALK to the user
+     * @dev Note: If there is not enough ALK, we do not perform the transfer all.
+     * @param user The address of the user to transfer ALK to
+     * @param amount The amount of ALK to (possibly) transfer
+     * @return The amount of ALK which was NOT transferred to the user
+     */
+    function transferAlkInternal(address user, uint amount) internal returns (uint) {}
 
-    function claimRewards(address holder, uint claimedAmount) public {
-        uint unclaimedRewards = calculateUnclaimedRewards(holder);
-        require(unclaimedRewards >= claimedAmount, "The unclaimed rewards must be greater than or equal to the given claimed amount");
-        alreadyClaimedRewards[holder] = alreadyClaimedRewards[holder] + claimedAmount;
-        transferRewards(holder, claimedAmount);
-    }
-
-
-    function getBlockNumber() public view returns (uint) {
+    function getCurrentBlockNumber() public view returns (uint) {
         return block.number;
     }
 
-    function getRewardAllocationRatioForMarket(address market) internal returns (uint) {// return 0.25
-        uint totalLiquidity = 0;
-        for (uint i = 0; i < ALL_MARKETS.length; i++) {
-            Market currentMarket = moneyMarket.markets[ALL_MARKETS[i]];
-            totalLiquidity = totalLiquidity + currentMarket.totalSupply + currentMarket.totalBorrows;
-        }
-        Market targetMarket = moneyMarket.markets[market];
-        return (targetMarket.totalSupply + targetMarket.totalBorrows) / totalLiquidity ;
+    function getAlkAccrued(address participant) public view returns (uint) {
+        return alkAccrued[participant];
     }
 
-    function transferRewards(address holder, uint amount) internal {
-        // @TODO
+    /**
+     * Return the address of the ALK token
+     * @return The address of ALK
+     */
+    function getAlkAddress() public view returns (address) {
+        return 0xc00e94Cb662C3520282E6f5717214004A7f26888;
     }
 
 }
