ec292b4237909a9227d6c0be3bb11c021934fc97
Part Saelim <part@atato.com>
2021-05-18 18:20:57 +0700

    initially add unit test for refresh alk speeds (2)

--- test/RewardControlHarnessTest.js
+++ test/RewardControlHarnessTest.js
@@ -1,17 +1,9 @@
 "use strict";
 
-// import {deployMockContract, MockProvider, solidity, deployContract} from 'ethereum-waffle';
-// import {expect, use} from 'chai';
-// import MoneyMarket from '../build/MoneyMarket.json'
-// import RewardControl from '../build/RewardControlHarness.json'
 const {deployMockContract, MockProvider, solidity, deployContract} = require('ethereum-waffle');
 const {expect, use} = require('chai');
 const MoneyMarket = require('../build/contracts/MoneyMarket.json');
 const RewardControl = require('../build/contracts/RewardControlHarness.json');
-
-const {gas} = require('./Utils');
-const {getContract} = require('./Contract');
-// const RewardControl = getContract("./RewardControlHarness.sol");
 
 use(solidity);
 
@@ -20,10 +12,6 @@ contract('RewardControl', function ([root, ...accounts]) {
 
     describe("#refreshAlkSpeeds", async () => {
         it("refresh ALK speeds when there is a single market", async () => {
-            /*const rewardControl = await RewardControl.new().send({from: root});
-            await rewardControl.methods.initializer(root, accounts[0], accounts[1]).send({gas: 1000000, from: root})
-            await rewardControl.methods.addMarket(accounts[2]).send({gas: 1000000, from: root})
-            await rewardControl.methods.harnessRefreshAlkSpeeds().send({gas: 1000000, from: root});*/
             const [a, b, c, d, e] = new MockProvider().getWallets();
             let mockMoneyMarket = await deployMockContract(a, MoneyMarket.abi);
             await mockMoneyMarket.mock.markets.returns(true, 0, e.address, 100, 0, 0, 100, 0, 0);
@@ -32,7 +20,7 @@ contract('RewardControl', function ([root, ...accounts]) {
             await rewardControl.addMarket(d.address, {gasPrice: GAS_PRICE});
             await rewardControl.harnessRefreshAlkSpeeds({gasPrice: GAS_PRICE});
             let alkSpeed = await rewardControl.alkSpeeds(d.address, {gasPrice: GAS_PRICE});
-            expect(alkSpeed.toString()).to.equals("8323820396000000000","ALK speed of a solely market should be according to ALK rate");
+            expect(alkSpeed.toString()).to.equals("8323820396000000000", "ALK speed of a solely market should be according to ALK rate");
         });
 
         it("refresh ALK speeds when there are two markets with the same alloc", async () => {
@@ -46,9 +34,25 @@ contract('RewardControl', function ([root, ...accounts]) {
             await rewardControl.addMarket(e.address, {gasPrice: GAS_PRICE});
             await rewardControl.harnessRefreshAlkSpeeds({gasPrice: GAS_PRICE});
             let dAlkSpeed = await rewardControl.alkSpeeds(d.address, {gasPrice: GAS_PRICE});
-            expect(dAlkSpeed.toString()).to.equals("4161910198000000000","ALK speed of market d should be half of ALK rate");
-            let eAlkSpeed = await rewardControl.alkSpeeds(d.address, {gasPrice: GAS_PRICE});
-            expect(eAlkSpeed.toString()).to.equals("4161910198000000000","ALK speed of market e should be half of ALK rate");
+            expect(dAlkSpeed.toString()).to.equals("4161910198000000000", "ALK speed of market d should be half of ALK rate");
+            let eAlkSpeed = await rewardControl.alkSpeeds(e.address, {gasPrice: GAS_PRICE});
+            expect(eAlkSpeed.toString()).to.equals("4161910198000000000", "ALK speed of market e should be half of ALK rate");
+        });
+
+        it("refresh ALK speeds when there are two markets with different alloc", async () => {
+            const [a, b, c, d, e] = new MockProvider().getWallets();
+            let mockMoneyMarket = await deployMockContract(a, MoneyMarket.abi);
+            await mockMoneyMarket.mock.markets.withArgs(d.address).returns(true, 0, e.address, 100, 0, 0, 100, 0, 0);
+            await mockMoneyMarket.mock.markets.withArgs(e.address).returns(true, 0, e.address, 150, 0, 0, 150, 0, 0);
+            let rewardControl = await deployContract(a, RewardControl);
+            await rewardControl.initializer(a.address, mockMoneyMarket.address, c.address, {gasPrice: GAS_PRICE});
+            await rewardControl.addMarket(d.address, {gasPrice: GAS_PRICE});
+            await rewardControl.addMarket(e.address, {gasPrice: GAS_PRICE});
+            await rewardControl.harnessRefreshAlkSpeeds({gasPrice: GAS_PRICE});
+            let dAlkSpeed = await rewardControl.alkSpeeds(d.address, {gasPrice: GAS_PRICE});
+            expect(dAlkSpeed.toString()).to.equals("3329528158400000000", "ALK speed of market d should be half of ALK rate");
+            let eAlkSpeed = await rewardControl.alkSpeeds(e.address, {gasPrice: GAS_PRICE});
+            expect(eAlkSpeed.toString()).to.equals("4994292237600000000", "ALK speed of market e should be half of ALK rate");
         });
     });
 
