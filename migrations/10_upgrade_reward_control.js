12a516081e76867135950b5e7ba5af155434b0b2
Part Saelim <part@atato.com>
2021-05-18 12:24:38 +0700

    initially add migrations for reward control

---
+++ migrations/10_upgrade_reward_control.js
@@ -0,0 +1,12 @@
+// migrations/4_prepare_upgrade_boxv2.js
+const RewardControl = artifacts.require("RewardControl");
+
+const { prepareUpgrade } = require("@openzeppelin/truffle-upgrades");
+
+module.exports = async function (deployer) {
+	const rewardControl = await RewardControl.deployed();
+	await prepareUpgrade(rewardControl.address, RewardControl, {
+		deployer,
+		unsafeAllowCustomTypes: true,
+	});
+};
