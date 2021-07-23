763b1c0a9e1196e01d3b2511f1661e930a4eeee7
Raghu <raghu@alkemi.ai>
2021-07-23 19:36:48 -0400

    Added check for paused oracle PROTO-76

--- contracts/AlkemiEarnVerified.sol
+++ contracts/AlkemiEarnVerified.sol
@@ -881,6 +881,10 @@ contract AlkemiEarnVerified is Exponential, SafeToken {
         if (oracle == address(0)) {
             return (Error.ZERO_ORACLE_ADDRESS, Exp({mantissa: 0}));
         }
+        
+        if (priceOracle.paused) {
+            return (Error.MISSING_ASSET_PRICE, Exp({mantissa: 0}));
+        }
 
         uint256 priceMantissa = priceOracle.getAssetPrice(asset);
 
